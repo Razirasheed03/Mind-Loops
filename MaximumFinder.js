740d24c14bc6621ad95fb77e365dc27c73f0af4f
Razirasheed03 <razirasheed03@gmail.com>
2025-05-06 17:29:51 +0530

    maximum finding using loops (no built in methods)

--- MaximumFinder.js
+++ MaximumFinder.js
@@ -4,6 +4,18 @@ function maxFinder(arr){
     let maximumVal=Math.max(...arr)
     return maximumVal
 }
-console.log(maxFinder([1,4,2,5,9,12]))
+// console.log(maxFinder([1,4,2,5,9,12]))
 
 //////////////// using loops /////////////////
+
+function MaxFind(arr){
+    let max=arr[0]
+    for(let i=1;i<arr.length;i++){
+        if(arr[i]>max){
+            max=arr[i]
+        }
+    }
+    return max
+}
+
+console.log(MaxFind([1,4,23,2,4,44]))
