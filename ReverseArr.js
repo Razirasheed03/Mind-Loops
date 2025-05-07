f2a53a8b89ad05f65b4439e476d6327d6da555a5
Razirasheed03 <razirasheed03@gmail.com>
2025-05-07 10:57:20 +0530

    reversing array with and without built in methods

---
+++ ReverseArr.js
@@ -0,0 +1,19 @@
+///////////with built in methods ////////////
+
+function revArray(arr){
+    let revArr=arr.reverse()
+    console.log(revArr)
+}
+revArray([1,2,3,4,5,])
+
+
+/////////// without built in methods ////////////
+
+function revArr(arr){
+    let newArr=[]
+    for(let i=arr.length-1;i>=0;i--){
+        newArr.push(arr[i])
+    }
+    console.log(newArr)
+}
+revArr([1,2,3,4,5])
