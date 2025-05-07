0aadbac702515d146ab00d64eb4827fb8794cdf2
Razirasheed03 <razirasheed03@gmail.com>
2025-05-07 17:43:40 +0530

    small changes

--- freqOfEach.js
+++ freqOfEach.js
@@ -1,13 +1,13 @@
-function findEach(arr){
-    let count={}
-    for(let i=0;i<arr.length;i++){
-        if(count[arr[i]]){
+function findEach(arr) {
+    let count = {}
+    for (let i = 0; i < arr.length; i++) {
+        if (count[arr[i]]) {
             count[arr[i]]++
-        }else{
-            count[arr[i]]=1
+        } else {
+            count[arr[i]] = 1
         }
     }
     return count
 }
 
-console.log(findEach([1,2,3,4,5,5]))
+console.log(findEach([1, 2, 3, 4, 5, 5]))
