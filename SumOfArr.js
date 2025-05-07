c8eb4cc65201308618f45938f5dc59dbfb85c944
Razirasheed03 <razirasheed03@gmail.com>
2025-05-07 11:30:28 +0530

    foramatted

--- SumOfArr.js
+++ SumOfArr.js
@@ -1,7 +1,12 @@
+
+////////////// with built in methods /////////////
+
 function sumOfArray(arr) {
     return arr.reduce((a, b) => a + b, 0);
 }
 
-
 const numbers = [1, 2, 3, 4, 5];
 console.log("Sum of array:", sumOfArray(numbers))
+
+
+///////////// without built in methods //////////////
