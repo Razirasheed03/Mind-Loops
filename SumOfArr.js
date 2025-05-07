0607f44f5ea6140735b5683a54ffcbab0d8995ac
Razirasheed03 <razirasheed03@gmail.com>
2025-05-07 11:32:01 +0530

    sum of array without built in methods

--- SumOfArr.js
+++ SumOfArr.js
@@ -10,3 +10,12 @@ console.log("Sum of array:", sumOfArray(numbers))
 
 
 ///////////// without built in methods //////////////
+
+function SumArr(arr){
+    let sum=0
+    for(let i=0;i<arr.length;i++){
+        sum+=arr[i]
+    }
+    return sum
+}
+console.log(SumArr([1,2,3,4,5]))
