f9185c21ff1afbf1199ebfafefb051e9f4996e35
Razirasheed03 <razirasheed03@gmail.com>
2025-05-07 10:24:45 +0530

    find minimum without using built in methods

--- MinimumFind.js
+++ MinimumFind.js
@@ -5,3 +5,18 @@ function findMin(arr){
     return min
 }
 // console.log(findMin([5,2,4,6,77,1]))
+
+
+///////////// using loops without built in /////////////////
+
+function findMinimum(arr){
+    let min=0;
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i]<min){
+                min=arr[i]
+            }
+        }
+    }
+    return arr
+}
