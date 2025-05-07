fef8bb4d2ef8e5ebeae0c7ca344fe9a57cd7fe12
Razirasheed03 <razirasheed03@gmail.com>
2025-05-07 17:42:51 +0530

    freq of each in an array

---
+++ freqOfEach.js
@@ -0,0 +1,13 @@
+function findEach(arr){
+    let count={}
+    for(let i=0;i<arr.length;i++){
+        if(count[arr[i]]){
+            count[arr[i]]++
+        }else{
+            count[arr[i]]=1
+        }
+    }
+    return count
+}
+
+console.log(findEach([1,2,3,4,5,5]))
