e9e28066e0d882a60eba61af2e036e04408c880c
Razirasheed03 <razirasheed03@gmail.com>
2025-05-08 17:15:36 +0530

    formatted pages

--- MinimumFind.js
+++ MinimumFind.js
@@ -1,7 +1,7 @@
 ////////////// using built in methods //////////////////
 
-function findMin(arr){
-    let min=Math.min(...arr)
+function findMin(arr) {
+    let min = Math.min(...arr)
     return min
 }
 // console.log(findMin([5,2,4,6,77,1]))
@@ -9,12 +9,12 @@ function findMin(arr){
 
 ///////////// using loops without built in /////////////////
 
-function findMinimum(arr){
-    let min=0;
-    for(let i=0;i<arr.length;i++){
-        for(let j=i+1;j<arr.length;j++){
-            if(arr[i]<min){
-                min=arr[i]
+function findMinimum(arr) {
+    let min = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] < min) {
+                min = arr[i]
             }
         }
     }
