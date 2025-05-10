bcda4a0d669de0b9f2ee8c2055ba988de877cf92
Razirasheed03 <razirasheed03@gmail.com>
2025-05-10 16:40:21 +0530

    made code cleaner and commented

--- ArrElemSame.js
+++ ArrElemSame.js
@@ -1,3 +1,5 @@
+//////////regardless to order ///////////
+
 function checkBothArrSame(arr1, arr2) {
     let first = arr1.join('')
     let second = arr2.join('')
@@ -8,3 +10,7 @@ function checkBothArrSame(arr1, arr2) {
     }
 }
 console.log(checkBothArrSame([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]))
+
+
+
+/////////// regarded to order //////////
