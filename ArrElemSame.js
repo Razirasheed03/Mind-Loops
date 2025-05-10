bc7626073fe4432fc4a860db2f6c99b87e48e441
Razirasheed03 <razirasheed03@gmail.com>
2025-05-10 16:43:08 +0530

    Array check same (regarded to the order)

--- ArrElemSame.js
+++ ArrElemSame.js
@@ -9,8 +9,19 @@ function checkBothArrSame(arr1, arr2) {
         return false
     }
 }
-console.log(checkBothArrSame([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]))
+// console.log(checkBothArrSame([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]))
 
 
 
 /////////// regarded to order //////////
+
+function checkBothArrElem(arr1, arr2) {
+    let first = arr1.sort().join('')
+    let second = arr2.sort().join('')
+    if (first === second) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(checkBothArrElem([1, 2, 4, 3,5 ], [1, 2, 3, 4, 5]))
