5a8915e358f2454c6e9439b5d96c060f29f1e08b
Razirasheed03 <razirasheed03@gmail.com>
2025-05-10 16:39:31 +0530

    small changes made

--- ArrElemSame.js
+++ ArrElemSame.js
@@ -1,10 +1,10 @@
-function checkBothArrSame(arr1,arr2){
-    let first=arr1.join('')
-    let second=arr2.join('')
-    if(first===second){
+function checkBothArrSame(arr1, arr2) {
+    let first = arr1.join('')
+    let second = arr2.join('')
+    if (first === second) {
         return true
-    }else{
+    } else {
         return false
     }
 }
-console.log(checkBothArrSame([1,2,3,4,5], [1,2,3,4,5]))
+console.log(checkBothArrSame([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]))
