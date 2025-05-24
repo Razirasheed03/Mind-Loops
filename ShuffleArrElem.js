80a00882fbd48df6c94c7494b29f8fe60d048982
Razirasheed03 <razirasheed03@gmail.com>
2025-05-24 23:37:24 +0530

    Shuffle array elements

---
+++ ShuffleArrElem.js
@@ -0,0 +1,10 @@
+let arr=['apple','car','bike','javaScript']
+
+function shuffleArr(arr){
+    for(let  i = 0 ;  i< arr.length ; i++){
+    let  c =Math.round(Math.random()*i);
+    [arr[i],arr[c]]=[arr[c],arr[i]];
+    }
+    return arr;
+}
+console.log(shuffleArr(arr))
