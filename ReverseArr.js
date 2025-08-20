f610c3bdca37a3cc32a5e92261ff0056e5d05325
Razirasheed03 <razirasheed03@gmail.com>
2025-08-20 22:48:38 +0530

    added new reverse method using unshift

--- ReverseArr.js
+++ ReverseArr.js
@@ -17,3 +17,16 @@ function revArr(arr) {
     console.log(newArr)
 }
 revArr([1, 2, 3, 4, 5])
+
+
+
+////////// using Shift /////////////
+
+function revArr(arr) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.unshift(arr[i])
+    }
+    console.log(newArr)
+}
+revArr([1, 2, 3, 4, 5])
