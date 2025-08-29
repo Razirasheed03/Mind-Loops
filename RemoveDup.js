d44914b2fbc83b59dbe31bd08def85eb8d6b0cb5
Razirasheed03 <razirasheed03@gmail.com>
2025-08-29 23:59:53 +0530

    small changes

--- RemoveDup.js
+++ RemoveDup.js
@@ -50,3 +50,17 @@ function removeDup(arr) {
     return res
 }
 console.log(removeDup([1, 3, 2, 3, 4, 4, 5]))
+
+
+function removeDup(arr) {
+    let seen = {}
+    let res = []
+    for (let val of arr) {
+        if (!seen[val]) {
+            seen[val] = true
+            res.push(val)
+        }
+    }
+    return res
+}
+console.log(removeDup([1, 3, 2, 3, 4, 4, 5]))
