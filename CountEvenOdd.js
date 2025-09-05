4391ffdb56baa970592e70446b8f3bc19a438fc9
Razirasheed03 <razirasheed03@gmail.com>
2025-09-05 23:12:16 +0530

    added Counting even and odd using filter method

--- CountEvenOdd.js
+++ CountEvenOdd.js
@@ -16,3 +16,12 @@ CountEvenOdd([2, 4, 6, 3, 1, 10])
 
 
 /////// using filter method /////////
+function CountEvenOdd(arr) {
+    let even = arr.filter(num => num % 2 === 0).length;
+    let odd = arr.filter(num => num % 2 !== 0).length;
+
+    console.log('count of even :', even);
+    console.log('count of odd :', odd);
+}
+
+CountEvenOdd([2, 4, 6, 3, 1, 10]);
