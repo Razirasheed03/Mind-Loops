d971e80580d353defc5c46e3974ee13060f799f0
Razirasheed03 <razirasheed03@gmail.com>
2025-09-10 23:38:56 +0530

    added date adding different method

--- DateAdd.js
+++ DateAdd.js
@@ -9,3 +9,9 @@ console.log(atoday.toDateString());
 
 
 ////// adding with milliseconds /////
+let today2 = new Date();
+
+// Add 15 days in milliseconds (1000ms * 60s * 60m * 24h * 15d)
+let futureDate = new Date(today2.getTime() + 15 * 24 * 60 * 60 * 1000);
+
+console.log(futureDate.toDateString());
