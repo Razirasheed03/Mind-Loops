e7a09c3f1e57fb9188059a45b273ae1e1245f748
Razirasheed03 <razirasheed03@gmail.com>
2025-09-11 23:51:04 +0530

    added finding frequencyCount of array element

--- FindFreq.js
+++ FindFreq.js
@@ -1,1 +1,15 @@
 //////// using object (hashmap) /////////
+function frequencyCount(arr) {
+  let freq = {};
+
+  for (let num of arr) {
+    freq[num] = (freq[num] || 0) + 1;
+  }
+
+  return freq;
+}
+
+// Example
+let nums = [2, 3, 2, 5, 3, 2, 1, 5];
+console.log(frequencyCount(nums));
+// Output: { '1': 1, '2': 3, '3': 2, '5': 2 }
