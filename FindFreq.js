c981256ca5d5823a12aca57e98c58394f080589a
Razirasheed03 <razirasheed03@gmail.com>
2025-09-12 19:06:54 +0530

    added frequency finding using map method

--- FindFreq.js
+++ FindFreq.js
@@ -14,4 +14,18 @@ console.log(frequencyCount(nums));
 
 
 ///// using map method /////
+function frequencyCount(arr) {
+  let freq = new Map();
+
+  for (let num of arr) {
+    freq.set(num, (freq.get(num) || 0) + 1);
+  }
+
+  return freq;
+}
+
+// Example
+// let nums = [2, 3, 2, 5, 3, 2, 1, 5];
+console.log(Object.fromEntries(frequencyCount(nums)));
+// Output: { '1': 1, '2': 3, '3': 2, '5': 2 }
 
