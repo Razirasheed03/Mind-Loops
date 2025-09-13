050f58506edf308844074b6a0dc98c2adf772ad0
Razirasheed03 <razirasheed03@gmail.com>
2025-09-13 22:14:43 +0530

    removed unwanted changes

--- FindFreq.js
+++ FindFreq.js
@@ -24,7 +24,7 @@ function frequencyCount(arr) {
   return freq;
 }
 
-// Example
+
 // let nums = [2, 3, 2, 5, 3, 2, 1, 5];
 console.log(Object.fromEntries(frequencyCount(nums)));
 // Output: { '1': 1, '2': 3, '3': 2, '5': 2 }
